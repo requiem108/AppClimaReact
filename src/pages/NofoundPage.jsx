6613f591d089799ef5d933ec3c578ac5af693412
Requiem108 <mori8307@gmail.com>
2021-05-11 21:18:48 -0500

    404

--- src/pages/NofoundPage.jsx
+++ src/pages/NofoundPage.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import Grid from '@material-ui/core/Grid'
+import { IconContext }  from 'react-icons'
+import { WiRain } from 'react-icons/wi'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+import {Link as RouterLink} from 'react-router-dom'
 
 const NofoundPage = () => {
   return (
-    <div>
-      No found 404 Mod
-      <div>
-        <Link to="/main">Ir a Main</Link>
+    <Grid container
+    direction='column'
+    justify ="center"        
+    className="full"            
+    >
+      <div className="higthlight">
+      <Grid item container xs={12}
+          justify="center"
+          alignItems ="center"
+        >
+          
+          <Grid item>
+            <IconContext.Provider value={{size:"6em"}}>
+                <WiRain/>
+            </IconContext.Provider>
+          </Grid>
+          <Grid item container direction='column'
+            justify="center" alignItems='center'>
+              <Typography variant="h4" color="inherit">
+                  404 | La pagina no existe
+              </Typography>
+              <Link color="inherit"
+                aria-label="menu"
+                component ={RouterLink}
+                to="/main">
+                VOLVER AL INICIO
+              </Link>
+          </Grid>
+        </Grid>
+
       </div>
-    </div>
+
+  </Grid>  
   )
 }
 
